f1cb4be98f083f1a5f8a9f6c330768331b362d89
jiaman <2433313140@qq.com>
2022-11-28 12:48:44 +0800

    修改自动填充字段拦截器

--- course-assistant-admin/src/main/java/com/zrn/assistant/common/handler/FieldMetaObjectHandler.java
+++ course-assistant-admin/src/main/java/com/zrn/assistant/common/handler/FieldMetaObjectHandler.java
@@ -3,8 +3,6 @@
 package com.zrn.assistant.common.handler;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
-import com.zrn.assistant.common.user.SecurityUser;
-import com.zrn.assistant.common.user.UserDetail;
 import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.stereotype.Component;
 
@@ -17,34 +15,20 @@ import java.util.Date;
 @Component
 public class FieldMetaObjectHandler implements MetaObjectHandler {
     private final static String CREATE_DATE = "createDate";
-    private final static String CREATOR = "creator";
     private final static String UPDATE_DATE = "updateDate";
-    private final static String UPDATER = "updater";
-    private final static String DEPT_ID = "deptId";
+
 
     @Override
     public void insertFill(MetaObject metaObject) {
-        UserDetail user = SecurityUser.getUser();
         Date date = new Date();
-
-        //创建者
-        strictInsertFill(metaObject, CREATOR, Long.class, user.getId());
         //创建时间
         strictInsertFill(metaObject, CREATE_DATE, Date.class, date);
-
-        //创建者所属部门
-        strictInsertFill(metaObject, DEPT_ID, Long.class, user.getDeptId());
-
-        //更新者
-        strictInsertFill(metaObject, UPDATER, Long.class, user.getId());
         //更新时间
         strictInsertFill(metaObject, UPDATE_DATE, Date.class, date);
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
-        //更新者
-        strictUpdateFill(metaObject, UPDATER, Long.class, SecurityUser.getUserId());
         //更新时间
         strictUpdateFill(metaObject, UPDATE_DATE, Date.class, new Date());
     }
