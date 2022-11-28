b7801b11eb008379bac5ab6c93a42599571debe9
jiaman <2433313140@qq.com>
2022-11-28 12:00:41 +0800

    修改mybatis-plus 配置

--- course-assistant-admin/src/main/java/com/zrn/assistant/common/config/MybatisPlusConfig.java
+++ course-assistant-admin/src/main/java/com/zrn/assistant/common/config/MybatisPlusConfig.java
@@ -21,14 +21,8 @@ public class MybatisPlusConfig {
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
-        // 数据权限
-        mybatisPlusInterceptor.addInnerInterceptor(new DataFilterInterceptor());
         // 分页插件
         mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
-        // 乐观锁
-        mybatisPlusInterceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
-        // 防止全表更新与删除
-        mybatisPlusInterceptor.addInnerInterceptor(new BlockAttackInnerInterceptor());
 
         return mybatisPlusInterceptor;
     }
