c6e856e05b3947c57aa7f0b56393489cc1df7c32
jiaman <2433313140@qq.com>
2022-12-01 19:24:22 +0800

    登录

--- course-assistant-front/teacher/vue.config.js
+++ course-assistant-front/teacher/vue.config.js
@@ -19,8 +19,13 @@ module.exports = {
     hotOnly: false,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true
+        target: 'http://127.0.0.1:8080',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/' 
+          //pathRewrite: {'^/api': '/'} 重写之后url为 http://192.168.1.16:8085/xxxx
+          //pathRewrite: {'^/api': '/api'} 重写之后url为 http://192.168.1.16:8085/api/xxxx
+        }
       }
     }
   },
